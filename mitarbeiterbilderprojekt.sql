72af6f936d1150168b708e4a7fb356f05a5347d5
peterbergerberlin <67024844+peterbergerberlin@users.noreply.github.com>
2020-06-17 16:40:01 +0200

    Update mitarbeiterbilderprojekt.sql

--- mitarbeiterbilderprojekt.sql
+++ mitarbeiterbilderprojekt.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Erstellungszeit: 11. Jun 2020 um 16:06
+-- Erstellungszeit: 17. Jun 2020 um 16:12
 -- Server-Version: 10.4.11-MariaDB
 -- PHP-Version: 7.4.5
 
@@ -33,7 +33,8 @@ CREATE TABLE `mitarbeiterbilder` (
   `vorname` varchar(100) NOT NULL,
   `nachname` varchar(100) NOT NULL,
   `position` varchar(100) NOT NULL,
-  `arbeitsplatz` varchar(100) NOT NULL,
+  `gebaeude` varchar(100) NOT NULL,
+  `raum` varchar(100) NOT NULL,
   `bilddateiname` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -41,13 +42,13 @@ CREATE TABLE `mitarbeiterbilder` (
 -- Daten für Tabelle `mitarbeiterbilder`
 --
 
-INSERT INTO `mitarbeiterbilder` (`id`, `userid`, `vorname`, `nachname`, `position`, `arbeitsplatz`, `bilddateiname`) VALUES
-(1, 1, 'Peter', 'Berger', 'Frontend Developer', 'Gebäude 2, Raum 208', '1_peter_berger.jpg'),
-(2, 2, 'Horst', 'Schlaemmer', 'Backend Developer', 'Gebaeude 2, Raum 207', '2_horst_schlaemmer.jpg'),
-(3, 3, 'Bernd', 'Stromberg', 'Chef vom Dienst', 'Gebaeude 2, Raum 310', '3_bernd_stromberg.jpg'),
-(14, 4, 'Linus', 'Neumann', 'Fullstack Developer und Psychologe', 'Gebäude 2, Raum 5a', 'fi_connect_Referenten_1170px_neumann.jpg'),
-(15, 5, 'Tim', 'Pritlove', 'Fullstack Developer und Podcaster', 'Gebäude 2, Raum 5b', 'tim_pritlove.jpg'),
-(21, 6, 'Jade', 'Raymond', 'Gamedeveloper', 'Gebäude 2, Raum 5c', 'jaderaymond-1200x900.jpg');
+INSERT INTO `mitarbeiterbilder` (`id`, `userid`, `vorname`, `nachname`, `position`, `gebaeude`, `raum`, `bilddateiname`) VALUES
+(1, 1, 'Peter', 'Berger', 'Frontend Developer', 'Gebäude 2', 'Raum 208', '1_peter_berger.jpg'),
+(2, 2, 'Horst', 'Schlaemmer', 'Backend Developer', 'Gebäude 2', 'Raum 207', '2_horst_schlaemmer.jpg'),
+(3, 3, 'Bernd', 'Stromberg', 'Chef vom Dienst', 'Gebäude 2', 'Raum 310', '3_bernd_stromberg.jpg'),
+(14, 4, 'Linus', 'Neumann', 'Fullstack Developer und Psychologe', 'Gebäude 2', 'Raum 5a', 'fi_connect_Referenten_1170px_neumann.jpg'),
+(15, 5, 'Tim', 'Pritlove', 'Fullstack Developer und Podcaster', 'Gebäude 2', 'Raum 5b', 'tim_pritlove.jpg'),
+(24, 6, 'Jade', 'Raymond', 'Gamedeveloper', 'Gebäude 2', 'Raum 5c', 'jaderaymond-1200x900.jpg');
 
 --
 -- Indizes der exportierten Tabellen
@@ -67,7 +68,7 @@ ALTER TABLE `mitarbeiterbilder`
 -- AUTO_INCREMENT für Tabelle `mitarbeiterbilder`
 --
 ALTER TABLE `mitarbeiterbilder`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
